5b3605cec5775efe31570b4d42b0b8b905fa4e1c
Adam <adamoctav@gmail.com>
2021-01-09 02:46:50 +0200

    add proposals: state, component

--- core/component.js
+++ core/component.js
@@ -11,8 +11,6 @@ import { setComponentDefaultState } from './component/state';
  */
 export const Component = (name, state) => {
 
-    let dr = null;
-
     return {
         name: name,
         children: [],
@@ -34,3 +32,98 @@ export const Component = (name, state) => {
         }
     };
 };
+
+/**
+ * https://flaviocopes.com/how-to-list-object-methods-javascript/
+ * 
+ */
+
+
+class _Component {
+    constructor(name) {
+        this.name = name;
+
+        this.__state = {
+            name: name,
+            children: [],
+            raw_template: document.querySelector(`#${name}`).innerHTML,
+            renderer: new SimpleObserver(true),
+            ready: function ( DataReceiver ) {
+                let component = setComponentDefaultState(
+                    name,
+                    state,
+                    DataReceiver,
+                    { internal: new SimpleObserver(false), external: this.renderer }
+                )
+    
+                DataReceiver.subscribe(name, (data) => {
+                    component.executeHook().onData(data)
+                });
+                component.render();
+                component.executeHook().onCreate()
+            }
+        };
+    }
+
+    prender() {
+        return {
+            name: this.name,
+            children: [],
+            raw_template: document.querySelector(`#${this.name}`).innerHTML,
+            renderer: new SimpleObserver(true),
+            ready: ( DataReceiver ) => {
+                let component = setComponentDefaultState(
+                    this.name,
+                    state,
+                    DataReceiver,
+                    { internal: new SimpleObserver(false), external: this.renderer }
+                )
+    
+                DataReceiver.subscribe(name, (data) => {
+                    component.executeHook().onData(data)
+                });
+                component.render();
+                component.executeHook().onCreate()
+            }
+        };
+    }
+}
+
+class MyCompo extends _Component {
+    constructor(){
+        super('my-component')
+        this.data = {
+            test:34,
+            lol:'LUL'
+        }
+    }
+}
+
+
+/*
+
+class A {
+    constructor(){
+        this.template = null;
+        this.y = null;
+    }
+    init(){
+        console.log(this.t,this.y)
+    }
+}
+
+class B extends A {
+
+    constructor(){
+        super();
+        this.t = 1;
+        this.y = 0;
+    }
+}
+
+let b = new B();
+b.init() // logs: y = 0, t = 1
+
+
+
+*/
--- core/component/state.js
+++ core/component/state.js
@@ -1,9 +1,20 @@
 import { Template } from "../template";
-import { SimpleObserver } from "../observer";
 import { DataProxy } from "./proxy";
 import { ExecuteHook, HOOKS } from "./hook";
 
 export class State {
+    constructor(name, state, dataReceiver) {
+        this.name = name
+        Object.assign(this, state.methods);
+        Object.assign(this, state.data);
+        this.component = {
+            sendMessageTo: (name, data) => dataReceiver.sendTo(name, data),
+            emitter: dataReceiver
+        }
+    }
+}
+
+export class InternalState {
     constructor(name, state, dataReceiver) {
         this.name = name
         Object.assign(this, state.methods);
@@ -54,4 +65,43 @@ export const setComponentDefaultState = (name, state, DataReceiver, renderer) =>
     _state.setTemplate();
     _state.setRendererSubscribers();   
     return _state;
+}
+
+class ExternalState {
+    constructor(name, state, DataReceiver, renderer) {
+        this.name = name;
+        this.internalState = new InternalState(name,state,DataReceiver)
+        this.template = {
+            instance: new Template(),
+            raw: document.querySelector(`#${name}`).innerHTML
+        }
+        this.hooks =  state.hooks,
+        this.renderer = {
+            internal: renderer.internal,
+            external: renderer.external
+        }
+        this.proxy =  DataProxy(this.internalState, this.renderer.internal);
+        this.setTemplate();
+        this.setRendererSubscribers();
+    }
+    setTemplate() {
+        this.template.instance.setTemplate(this.name, this.template.raw)
+    }
+    render() {
+        this.template.instance.render(this.name, this.proxy);
+    }
+    setRendererSubscribers() {
+        this.renderer.internal.subscribe(this.name, () => {
+            this.template.instance.render(this.name, this.proxy)
+        })
+        this.renderer.external.subscribe(this.name, () => {
+            this.template.instance.render(this.name, this.proxy)
+        })
+    }
+    executeHook() {
+        return {
+            onCreate: () => ExecuteHook(HOOKS.ON_CREATE, this.hooks, this.proxy),
+            onData: data => ExecuteHook(HOOKS.ON_DATA, this.hooks, this.proxy, data)
+        }
+    }
 }
